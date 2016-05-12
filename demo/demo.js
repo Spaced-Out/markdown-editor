a6210466b65c5e40c172bbd9d7d1cc6bcb86b5fe
Marcos Ojeda <subliminal@gmail.com>
2016-05-12 13:27:01 -0700

    initial markdown-editor commit (not building)

---
+++ demo/demo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarkdownEditor from '../index.js';
+
+
+ReactDOM.render(<MarkdownEditor />, document.getElementById('editor'));
---
+++ index.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import ExecutionEnvironment from 'exenv';
+
+
+let ProseMirror = () => {};
+if (ExecutionEnvironment.canUseDOM) {
+  // ProseMirror expects to access the DOM on require, so we
+  // gate the requires with exenv. Can't use import though because
+  // `import` needs to be a top level function. Ideally we could
+  // just import prosemirror outright, but it isn't coded for our
+  // environment... yet.
+  ProseMirror = require('prosemirror/dist/edit').ProseMirror;
+  require('prosemirror/dist/parse/markdown');
+  require('prosemirror/dist/serialize/markdown');
+  require('prosemirror/dist/menu/menubar');
+  require('prosemirror/dist/menu/inlinemenu');
+}
+
+
+export default class MarkdownEditor extends Component {
+  constructor(props) {
+    super(props);
+
+    // binding changeHandler to this so that ProseMirror's .off works
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  componentWillMount() {
+    this.val = (this.props.value ||
+      ('valueLink' in this.props && this.props.valueLink.value) ||
+      this.props.defaultValue) || '';
+
+    this.proseMirror = new ProseMirror({
+      place: this.refs.prosemirror,
+      doc: this.val,
+      docFormat: 'markdown',
+      menuBar: true,
+      inlineMenu: true,
+      buttonMenu: false,
+      label: this.props.label,
+    });
+  }
+
+  componentDidMount() {
+    this.refs.prosemirror.appendChild(this.proseMirror.wrapper);
+
+    this.proseMirror.on('change', this.changeHandler);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.newVal = (newProps.value ||
+      ('valueLink' in newProps && newProps.valueLink.value) ||
+      newProps.defaultValue) || '';
+  }
+
+  componentWillUpdate() {
+    // unlike with most controlled inputs, we only setContent when
+    // the value was changed by an external source (and not the user
+    // typing in the proseMirror field directly).
+    if (this.newVal !== this.val) {
+      this.val = this.newVal;
+      this.proseMirror.setContent(this.val, 'markdown');
+    }
+  }
+
+  componentWillUnmount() {
+    this.proseMirror.off('change', this.changeHandler);
+  }
+
+  render() {
+    const className = this.props.className || 'editor-description';
+
+    return <div className={className}>
+      <Placeholder text={this.props.label} value={this.val} />
+      <div ref="prosemirror" />
+    </div>;
+  }
+
+  changeHandler() {
+    let callback = (this.props.onChange ||
+      'valueLink' in this.props && this.props.valueLink.requestChange);
+    if (callback) {
+      this.val = this.proseMirror.getContent('markdown');
+      callback(this.val);
+    }
+  }
+
+  getContent(type = 'markdown') {
+    return this.proseMirror.getContent(type);
+  }
+}
+
+MarkdownEditor.propTypes = {
+  value: React.PropTypes.string,
+  valueLink: React.PropTypes.object,
+  onChange: React.PropTypes.func,
+  defaultValue: React.PropTypes.string,
+  label: React.PropTypes.string,
+  className: React.PropTypes.string,
+};
+
+
+let Placeholder = (props) => {
+  let {value, text} = props;
+
+  value = (value || '').trim();
+  return <div className={'placeholder-component' + (value ? ' placeholder-component-active': '')}>
+    {text}
+  </div>;
+};
